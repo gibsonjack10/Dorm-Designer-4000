1bd8dcf76cf5375a1cbf13526afdf7af9d1dbc3c
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-02-21 17:26:24 -0600

    update (loads but has extra print statements)

--- LoadButton.java
+++ LoadButton.java
@@ -5,11 +5,11 @@ import java.io.FileNotFoundException;
 public class LoadButton {
 	private static final int WIDTH = 96;
 	private static final int HEIGHT = 32;
-
 	private PApplet processing;
 	private float[] position;
 	private String label;
 	private File f;
+	private File g;
 
 	public LoadButton(float x, float y, PApplet processing) {
 		this.processing = processing;
@@ -19,11 +19,6 @@ public class LoadButton {
 		label = new String("Load Room");
 	}
 
-
-
-
-
-
 	public void update() {
 		if (isMouseOver() == true) {
 			processing.fill(100);
@@ -31,7 +26,6 @@ public class LoadButton {
 			processing.fill(200);
 		}
 		processing.rect(position[0] - WIDTH/2, position[1] + HEIGHT/2, position[0] + WIDTH/2, position[1] - HEIGHT/2);
-
 		processing.fill(0);
 		processing.text(label, position[0], position[1]);
 	}
@@ -39,34 +33,34 @@ public class LoadButton {
 	public Furniture mouseDown(Furniture furniture[]) throws FileNotFoundException {
 		try {
 			if (isMouseOver()) {
-				f = new File("RoomData3.ddd");
+				f = new File("RoomData.ddd");
 				Scanner scan = new Scanner(f);
 				String finalLines = new String();
 				int i = 0;
-				while(scan.hasNextLine() && i < 6) {
+				for (int j=0; j<6; j++)
+					furniture[j] = null;
+				while(scan.hasNextLine()) {
+					if (i == 6) {
+						System.out.println("WARNING: Unable to load more furniture.");
+						break;
+					}
 					finalLines = scan.nextLine();
 					String trimLine = finalLines.trim();
 					String furnType = trimLine.split(":")[0];
+					g = new File(furnType + ".png");
+					System.out.println(g);
+					if (g.exists()) {
+						System.out.println("WARNING: Could not find an image for a furniture object of type: " + furnType);
+						finalLines = scan.nextLine();
+					}
 					String numbers = trimLine.split(":")[1];
 					String xPos = numbers.split(",")[0];
 					String yPos = numbers.split(",")[1];
 					String rotations = numbers.split(",")[2];
-					System.out.println(furnType + numbers);
-					furniture[i] = null;
-					
-					furniture[i] = new Furniture(furnType, Float.parseFloat(xPos), Float.parseFloat(yPos),
-							Integer.parseInt(rotations), processing);
+					furniture[i] = new Furniture(furnType, Float.parseFloat(xPos), Float.parseFloat(yPos), Integer.parseInt(rotations), processing);
 					i++;
-					
-//					return furniture[i-1];
-					
-					//System.out.println(scan.nextLine());
-					//finalLines = scan.nextLine();
 				}
-//				for(int i = 0; i < 6; i++) {
-//					furniture[i] = new Furniture(label, i, i, i, processing);
-//				}
-
+				scan.close();
 			} else {
 				return null;
 			}
@@ -74,7 +68,6 @@ public class LoadButton {
 			
 		}
 		return null;
-
 	}
 
 	public boolean isMouseOver() {
