70ca91bfcac1357dad54f6a621ec344c83310d88
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-02-21 18:32:43 -0600

    need exception 1

--- SaveButton.java
+++ SaveButton.java
@@ -1,22 +1,17 @@
 import java.io.File;
 import java.io.PrintWriter;
+//import java.util.Scanner;
 import java.io.FileNotFoundException;
 
-/**
- * (This class creates a button to make beds.)
- *
- * <p>Bugs: (no known bugs)
- *
- * @author (Bennett Majerowski and John Gibson)
- */
 public class SaveButton {
 	private static final int WIDTH = 96;
 	private static final int HEIGHT = 32;
+	 
 	private PApplet processing;
 	private float[] position;
 	private String label;
 	private File f;
-//	private File myOutFile;
+	//private File myOutFile;
 	
 	public SaveButton(float x, float y, PApplet processing) {
 		this.processing = processing;
@@ -26,10 +21,11 @@ public class SaveButton {
 		label = new String("Save Room");
 	}
 	
-	/**
-	 * (update prints the button on the java window that can be clicked
-	 * to create new bed objects.)
-	 */
+	
+	
+	
+	
+	
 	public void update() {
 		if (isMouseOver() == true) {
 			processing.fill(100);
@@ -42,29 +38,23 @@ public class SaveButton {
 		processing.text(label, position[0], position[1]);
 	}
 	
-	/**
-	 * (mouseDown identifies the type "bed" if the mouse is clicked while above the 
-	 * Create sofa button.)
-	 */
-	public void mouseDown(Furniture furniture[]) throws FileNotFoundException { 
+	public void mouseDown(Furniture furniture[]) throws FileNotFoundException{
 		if (isMouseOver()) {
 			f = new File("RoomData.ddd");
 			PrintWriter pw = new PrintWriter(f);
-			for (int i=0; i<furniture.length; i++) {
-				if (furniture[i] != null) {
-					pw.println(furniture[i].getType() + ":" + furniture[i].getXPosition() + "," + furniture[i].getYPosition() + "," + furniture[i].getRotation());
-				} else {
+			for (int i = 0; i < furniture.length; i++) {
+				if (furniture[i] == null) {
 					pw.println();
+				} else {
+					pw.println(furniture[i].getType() + ":" + furniture[i].getXPosition() + "," +
+							 furniture[i].getYPosition() + "," + furniture[i].getRotation());
 				}
-				pw.close();
-			}
+				
+			}	
+			pw.close();
 		}
 	}
 	
-	/**
-	 * (isMouseOver is a helper method that identifies when the mouse is 
-	 * over the Create Bed button.)
-	 */
 	public boolean isMouseOver() {
 		if (processing.mouseX > position[0] - WIDTH/2 && 
 				processing.mouseX < position[0] + WIDTH/2 &&
@@ -74,6 +64,4 @@ public class SaveButton {
 		}
 		return false;	
 	}
-
-	
 }
