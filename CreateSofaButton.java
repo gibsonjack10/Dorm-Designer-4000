cfc1c03416e3e4582e946d1a904251cc7372b8c2
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-02-22 03:56:33 -0600

    update final createsofabutton

--- CreateSofaButton.java
+++ CreateSofaButton.java
@@ -1,16 +1,23 @@
+/**
+ * (This class creates a button to make beds.)
+ *
+ * <p>Bugs: (no known bugs)
+ *
+ * @author (Bennett Majerowski and John Gibson)
+ */
 public class CreateSofaButton {
-
 	private static final int WIDTH = 96;
-	private static final int HEIGHT = 32;
-	 
+	private static final int HEIGHT = 32; 
 	private PApplet processing;
 	private float[] position;
 	private String label;
 	 
-	
-	
-	
-	
+	/**
+	 * Constructor that creates the bed button
+	 * @param x is the horizontal location of the button
+	 * @param y is the vertical location of the button
+	 * @param processing uses information from the jar file
+	 */
 	public CreateSofaButton(float x, float y, PApplet processing) {
 		this.processing = processing;
 		position = new float[2];
@@ -19,10 +26,10 @@ public class CreateSofaButton {
 		label = new String("Create Sofa");
 	}
 	
-	
-	
-	
-	
+	/**
+	 * (update prints the button on the java window that can be clicked
+	 * to create new sofa objects.)
+	 */
 	public void update() { 
 		if (isMouseOver() == true) {
 			processing.fill(100);
@@ -35,25 +42,24 @@ public class CreateSofaButton {
 		processing.text(label, position[0], position[1]);
 	}
 	
-	
-	
-	
-	
+	/**
+	 * (mouseDown identifies the type "sofa" if the mouse is clicked while above the 
+	 * Create sofa button.)
+	 */
 	public Furniture mouseDown() {
 		if (isMouseOver()) {
 			Furniture sofa = new Furniture("sofa", processing);
-			
 			return sofa;
 		}
 		else {
 			return null;
 		}
-	} // After step 10, this method will instead return Furniture	
+	}
 	
-	
-	
-	
-	
+	/**
+	 * (isMouseOver is a helper method that identifies when the mouse is 
+	 * over the Create Sofa button.)
+	 */
 	public boolean isMouseOver() {
 		if (processing.mouseX > position[0] - WIDTH/2 && 
 				processing.mouseX < position[0] + WIDTH/2 &&
@@ -62,6 +68,7 @@ public class CreateSofaButton {
 			return true;
 		}
 		return false;
-		
 	}	
+	
+	
 }
